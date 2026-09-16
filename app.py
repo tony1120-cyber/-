38579b24f083a637a30259fce242a8f96509d4ae
tony1120-cyber <tony1120@goedu.kr>
2026-09-16 15:26:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -411,7 +411,7 @@ if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 5000))
-    )            ]
+    )            
         )
 
         return jsonify({
